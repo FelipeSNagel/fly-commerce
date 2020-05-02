702b7c606e717cd243541cc8a21ad48dd7e728b4
Felipe Nagel <felipesnagel@gmail.com>
2020-05-02 17:53:19 -0300

    Ajustes Mobile

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
+
 import { createAppContainer, createSwitchNavigator  } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
@@ -34,33 +34,5 @@ const Auth = createSwitchNavigator({
 });
 
 const Routes = createAppContainer(Auth);
-=======
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-
-import Home from './pages/home/';
-import Cart from './pages/cart';
-import Header from './components/Header';
-
-import colors from './styles/colors';
-
-const Routes = createAppContainer(
-  createStackNavigator(
-    {
-      Home,
-      Cart,
-    },
-    {
-      // initialRouteName: 'Cart',
-      defaultNavigationOptions: navigation => ({
-        header: () => <Header {...navigation} />,
-        cardStyle: {
-          backgroundColor: colors.dark,
-        },
-      }),
-    }
-  )
-);
->>>>>>> 5578f387cb9b153d153dbbc58378b48d2423b0f0
 
 export default Routes;
